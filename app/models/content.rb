ff7d5ced20d1227d8865d377a8c320df775d133a
vsting <faetonvs@gmail.com>
2014-03-16 21:11:25 +0600

    master 16.03.2014 21.11

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,7 +1,6 @@
 class Content < ActiveRecord::Base
   attr_accessible :child_menu_id, :post, :title, :assets_attributes
   belongs_to :child_menu
-
 	has_many :assets
 	accepts_nested_attributes_for :assets, :allow_destroy => true
 
