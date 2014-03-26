ba09e3c01a06c437bbfaab402e552445a944e3e5
vsting <faetonvs@gmail.com>
2014-03-26 10:25:29 +0600

    m delete

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,7 @@
 class Asset < ActiveRecord::Base
-  attr_accessible :asset, :asset_content_type, :asset_file_name, :asset_file_size, :asset_updated_at, :content_id
+  attr_accessible :asset, :asset_content_type, :asset_file_name, :asset_file_size, :asset_updated_at, :content_id, :assets_attributes
 	belongs_to :content
 	has_attached_file :asset
-	do_not_validate_attachment_file_type :asset
+  validates_attachment_content_type :asset, :content_type => [ 'text/plain', 'application/pdf', 'application/octet-stream', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ]
+	#do_not_validate_attachment_file_type :asset
 end
--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,6 @@ class Content < ActiveRecord::Base
   attr_accessible :child_menu_id, :post, :title, :assets_attributes
   belongs_to :child_menu
 	has_many :assets
-	accepts_nested_attributes_for :assets, :allow_destroy => true
+	accepts_nested_attributes_for :assets
 
 end
